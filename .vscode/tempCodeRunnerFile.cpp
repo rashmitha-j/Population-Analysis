803b455309677913a5f3435249732d98cdeb78a2
rashmitha15219 <rashmithajagadish1@gmail.com>
2025-10-02 11:47:55 +0530

    Initial commit: Task 01 population analysis

--- .vscode/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    double price;
-    cout << "Enter the price:";
-    cin >> price;
-    cout << "You Entered price =" << price << endl;
-    return 0;
-}
